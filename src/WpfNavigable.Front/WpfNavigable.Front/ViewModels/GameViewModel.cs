56d198c6c625569b67f7c2d52d1d4b065b65f4c3
ambsenyestudi <adrifre7@hotmail.com>
2021-03-11 18:23:05 +0100

    Game play

--- src/WpfNavigable.Front/WpfNavigable.Front/ViewModels/GameViewModel.cs
+++ src/WpfNavigable.Front/WpfNavigable.Front/ViewModels/GameViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using System;
+using System.IO;
 using System.Windows.Input;
 using WpfNavigable.Front.ViewModels.Base;
 
@@ -40,6 +41,21 @@ namespace WpfNavigable.Front.ViewModels
             {
                 TL="X";
             }
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            path += @"\TicTacToe";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            var key = "123456789";
+            var filename = $@"{path}\WriteFile.txt";
+            var encryptedText = EncryptionHelper.Encrypt("Hello world", key);
+            File.WriteAllText(filename, encryptedText);
+            var readText = File.ReadAllText(filename);
+            Console.WriteLine(readText);
+            var regularText = EncryptionHelper.Decrypt(readText, key);
+            Console.WriteLine(regularText);
+
         }
 
         private bool CanPlayPosition(string position) =>
